b1921908de00b39673a2bd8e14349d285882109a
Vernon Burt <ChargerIIC@yahoo.com>
2017-04-20 12:07:44 -0700

    Highlight now wrapped by Selectable
    This may seem an odd change, but I dislike having so much code in a
    customized third party script. If the script ever gets updated I could
    lose the changes.

---
+++ Assets/Scripts/Figure/Selectable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that marks any figure that can be selected in the Game UI
+/// Inhits from the Third Party Project Highlight
+/// </summary>
+public class Selectable : HighLight
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool IsSelected
+    {
+        get { return this.isHighlighted; }
+    }
+}
