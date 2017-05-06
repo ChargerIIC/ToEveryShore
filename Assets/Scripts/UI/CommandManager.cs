9957c09e8896fcf32a98a77558db84e4b0276b2b
Vernon Burt <ChargerIIC@yahoo.com>
2017-05-05 22:02:34 -0700

    Hiding buttons when not in use instead of enable/disable
    Much more user friendly

--- Assets/Scripts/UI/CommandManager.cs
+++ Assets/Scripts/UI/CommandManager.cs
@@ -21,13 +21,13 @@ public class CommandManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    Button1.enabled = false;
-	    Button2.enabled = false;
-	    Button3.enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Button1.gameObject.SetActive(false);
+	    Button2.gameObject.SetActive(false);
+	    Button3.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -45,15 +45,15 @@ public class CommandManager : MonoBehaviour
         SelectedFigure.gameObject.AddComponent<ShootInputController>();
 
         //TODO: Button for each weapon
-        Button1.enabled = false;
+        Button1.gameObject.SetActive(false);
         Button1.GetComponentInChildren<Text>().text = "";
 
-        Button2.enabled = true;
+        Button2.gameObject.SetActive(true);
         Button2.GetComponentInChildren<Text>().text = "Open Fire";
         Button2.onClick.RemoveAllListeners();
         Button2.onClick.AddListener(selectedFigureOpenFire); //Why the hell doesn't unity accept the normal += sytax ?
 
-        Button3.enabled = true;
+        Button3.gameObject.SetActive(true);
         Button3.GetComponentInChildren<Text>().text = "Cancel";
         Button3.onClick.RemoveAllListeners();
         Button3.onClick.AddListener(resetMenu);
@@ -77,16 +77,16 @@ public class CommandManager : MonoBehaviour
         moveController.RingPreFab = this.RingPrefab;
         moveController.WayPointPrefab = this.WaypointPrefab;
 
-        Button1.enabled = true;
+        Button1.gameObject.SetActive(true);
         Button1.GetComponentInChildren<Text>().text = "Move";
         Button1.onClick.RemoveAllListeners();
         Button1.onClick.AddListener(selectedFigureMove); //Why the hell doesn't unity accept the normal += sytax ?
 
-        Button2.enabled = true;
+        Button2.gameObject.SetActive(true);
         Button2.GetComponentInChildren<Text>().text = "Reset";
         Button2.onClick.RemoveAllListeners();
 
-        Button3.enabled = true;
+        Button3.gameObject.SetActive(true);
         Button3.GetComponentInChildren<Text>().text = "Cancel";
         Button3.onClick.RemoveAllListeners();
         Button3.onClick.AddListener(resetMenu);
@@ -106,16 +106,17 @@ public class CommandManager : MonoBehaviour
 
     void resetMenu()
     {
+
         //TODO: Update for figure's available actions
-        Button1.enabled = true;
+        Button1.gameObject.SetActive(true);
         Button1.GetComponentInChildren<Text>().text = "Move";
         Button1.onClick.AddListener(setupMoveButton);
 
-        Button2.enabled = true; 
+        Button2.gameObject.SetActive(true);
         Button2.GetComponentInChildren<Text>().text = "Shoot";
         Button2.onClick.AddListener(setupShootingButton);
 
-        Button3.enabled = true;
+        Button3.gameObject.SetActive(true);
         Button3.GetComponentInChildren<Text>().text = "Ability";
         Button3.onClick.AddListener(setupAbilityButton);
     }
